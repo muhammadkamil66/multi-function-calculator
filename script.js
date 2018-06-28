f5bf423d001a26d74afc0aa0a24a7bf74c702898
Muhammad Kamil <38943329+muhammadkamil66@users.noreply.github.com>
2018-06-28 17:59:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let a = [12,23,45];
+let b = ["Kamil", "Faisal", "Ahsan"];
+
+
+
+// a.pop();
+// console.log(a[2]);
+// console.log(a.length);
+
+
+
+
+
+
+a.push(1234);
+// console.log(a[2]);
+// console.log(a.length);
+
+for(let i=0; i < a.length; i +=1){
+	console.log(i[a]);
+}
